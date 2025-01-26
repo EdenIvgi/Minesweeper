1eefcfb268ab52d62cf5417b29c9fac77deb1fff
EdenIvgi <edenavgi@gmail.com>
2025-01-26 21:29:34 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,35 @@
 'use strict'
+
+
+function onInit() {
+
+}
+
+function buildBoard() {
+
+}
+
+function setMinesNegsCount(board) {
+
+}
+
+function renderBoard(board) {
+
+}
+
+function onCellClicked(elCell, i, j) {
+
+}
+
+function onCellMarked(elCell) {
+
+}
+
+function checkGameOver() {
+
+}
+
+function expandUncover(board, elCell, i, j) {
+
+}
+
